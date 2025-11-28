335a7878300661c834395b3e9eae4db2969d0ba0
Calum Heldt <cal.muon32@gmail.com>
2025-11-27 23:46:24 -0600

    error

--- solvers_2d/navier_stokes/MMS.py
+++ solvers_2d/navier_stokes/MMS.py
@@ -1,7 +1,7 @@
 from firedrake import *
 import matplotlib.pyplot as plt
 from solvers_2d.timestepper_MMS import timestepper_MMS
-from make_weak_form import make_weak_form
+from solvers_2d.navier_stokes.make_weak_form import make_weak_form
 import numpy as np
 
 # constants
