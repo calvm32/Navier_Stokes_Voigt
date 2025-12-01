6c1fc4e3d6743d57f4904d8c9561c3a81654c0e1
Calum Heldt <cal.muon32@gmail.com>
2025-12-01 10:07:23 -0600

    dt =1

--- solvers_2d/navier_stokes/config.py
+++ solvers_2d/navier_stokes/config.py
@@ -2,7 +2,7 @@ from firedrake import *
 
 # constants
 T = 2           # final time
-dt = 0.1        # timestepping length
+dt = 1        # timestepping length
 theta = 1/2     # theta constant
 N = 10          # mesh size
 
