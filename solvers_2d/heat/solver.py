9b610d320b9f050c905f4b3febe82f0f803512b7
Calum Heldt <cal.muon32@gmail.com>
2025-12-02 15:52:57 -0600

    slightly change solve file

--- solvers_2d/heat/solver.py
+++ solvers_2d/heat/solver.py
@@ -3,12 +3,18 @@ from solvers_2d.timestepper import timestepper
 from .make_weak_form import make_weak_form
 from .config import t0, T, dt, theta, N
 
+
+blue(f"\n*** Starting solve ***\n", spaced=True)
+
 # mesh
 mesh = UnitSquareMesh(N, N)
 x, y = SpatialCoordinate(mesh)
 
+t = Constant(0.0) # symbolic constant for t
+ufl_exp = ufl.exp # ufl e, so t gets calculated correctly 
+
 # functions
-ufl_u0 = Constant(0)            # initial condition u0
+ufl_u0 = ufl_exp(t)*cos(pi*x)   # initial condition u0
 ufl_f = cos(x*pi)*cos(y*pi)     # source term f
 ufl_g = Constant(0)             # bdy condition g
 
