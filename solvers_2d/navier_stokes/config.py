cf29eec2e21322a6f0871394db2b798b5c25c3ed
Calum Heldt <cal.muon32@gmail.com>
2025-12-02 11:52:04 -0600

    N starts at 4

--- solvers_2d/navier_stokes/config.py
+++ solvers_2d/navier_stokes/config.py
@@ -5,7 +5,7 @@ from firedrake import *
 # ---------
 
 T = 1                   # final time
-dt = 0.001              # timestepping length
+dt = 0.001               # timestepping length
 theta = 1/2             # theta constant
 Re = Constant(0.01)     # Reynold's num for viscosity
 
@@ -24,6 +24,6 @@ H = 1.0                 # height of rectangle, with length = 3H
 
 # MMS loops over mesh sizes in this list
 N_list = []
-for exp in range(3, 7):
+for exp in range(4, 7):
     N = 2**exp
     N_list.append(N)
