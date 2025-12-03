e451cdeab93c39402b66bd8e1b9b0ed504a6e26b
Calum Heldt <cal.muon32@gmail.com>
2025-12-02 19:44:48 -0600

    added monitors

--- solvers_2d/navier_stokes/config.py
+++ solvers_2d/navier_stokes/config.py
@@ -29,10 +29,10 @@ solver_parameters = {
     "fieldsplit_1_pcd_Mp_pc_type": "lu",
     "fieldsplit_1_pcd_Kp_pc_type": "lu",
     "fieldsplit_1_pcd_Fp_mat_type": "matfree",
-    #"snes_monitor": None,
-    #"snes_converged_reason": None,
-    #"ksp_monitor_true_residual": None,
-    #"ksp_converged_reason": None,
+    "snes_monitor": None,
+    "snes_converged_reason": None,
+    "ksp_monitor_true_residual": None,
+    "ksp_converged_reason": None,
 }
 
 # ----------------
