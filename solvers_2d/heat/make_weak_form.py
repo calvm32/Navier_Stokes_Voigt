af2576f9ae42a6776ee77add25e567aa68e09870
Calum Heldt <cal.muon32@gmail.com>
2025-12-03 13:31:39 -0600

    make sure weak form returns F

--- solvers_2d/heat/make_weak_form.py
+++ solvers_2d/heat/make_weak_form.py
@@ -12,7 +12,8 @@ def make_weak_form(theta, idt, f, f_old, g, g_old, dsN):
 
     def F(u, u_old, v):
         u_mid = theta*u + (1-theta)*u_old
-        F = ( idt*(u - u_old)*v*dx
+
+        return ( idt*(u - u_old)*v*dx
             + inner(grad(u_mid), grad(v))*dx
             - f_mid*v*dx - g_mid*v*dsN
         )
