697ac172ac8f844718919536e18c2871fad0554d
Calum Heldt <cal.muon32@gmail.com>
2025-12-03 19:18:04 -0600

    back to prev get_data

--- solvers_2d/navier_stokes/solver.py
+++ solvers_2d/navier_stokes/solver.py
@@ -20,41 +20,23 @@ V = VectorFunctionSpace(mesh, "CG", 2)
 W = FunctionSpace(mesh, "CG", 1)
 Z = V * W
 
-solver_parameters["appctx"]["velocity_space"] = V # Z.sub(0).topological.ufl_function_space() # necessary so it's a function space
+solver_parameters["appctx"]["velocity_space"] = Z.sub(0).topological.ufl_function_space() # necessary so it's a function space
 
-# allocate Functions for timestepping
-u_old = Function(Z, name="u_old")
-u_new = Function(Z, name="u_new")
-
-# Initialize velocity and pressure as real Functions
-u_old.sub(0).interpolate(as_vector([sin(pi*x), cos(pi*y)]))  # velocity
-u_old.sub(1).interpolate(Constant(5.0))                       # pressure
-
-# Make "get_data" return Functions instead of symbolic UFL expressions
+# time dependant
 def get_data(t):
     
     # functions
-    ufl_v0 = as_vector([t, cos(pi*y)])    # velocity ic
-    ufl_p0 = Constant(5.0)                # pressure ic
-    ufl_f = as_vector([0, 0])             # source term f
-    ufl_g = as_vector([0, 0])             # bdy condition g
+    ufl_v0 = as_vector([sin(pi*x), cos(pi*y)])    # velocity ic
+    ufl_p0 = Constant(5.0)                          # pressure ic
+    ufl_f = as_vector([0, 0])                       # source term f
+    ufl_g = as_vector([0, 0])                       # bdy condition g
 
-    # Allocate functions
-    v = Function(V)
-    p = Function(W)
-    f = Function(V)
-    g = Function(V)
-    
-    # Interpolate ufl symbolic expressions
-    v.interpolate(ufl_v0)
-    p.interpolate(ufl_p0)
-    f.interpolate(ufl_f)
-    g.interpolate(ufl_g)
-    
-    return {"ufl_v0": v,
-            "ufl_p0": p,
-            "ufl_f": f,
-            "ufl_g": g}
+    # returns
+    return {"ufl_v0": ufl_v0,
+            "ufl_p0": ufl_p0,
+            "ufl_f": ufl_f,
+            "ufl_g": ufl_g,
+            }
 
 # BCs from demo
 bcs = [DirichletBC(Z.sub(0), Constant((1, 0)), (4,)),
