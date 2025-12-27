d3d018491f32db62212447ec195c9f93495eb1d3
Calum Heldt <cal.muon32@gmail.com>
2025-12-27 15:55:37 -0700

    correctly formatted rect mesh

--- solvers/navier_stokes_2d/solver_MMS.py
+++ solvers/navier_stokes_2d/solver_MMS.py
@@ -18,7 +18,7 @@ for N in N_list:
     # Setup spaces
     # ------------
 
-    mesh = RectangleMesh(H, L)
+    mesh = RectangleMesh(H, L, N*H, N*L)
     x, y = SpatialCoordinate(mesh)
 
     dx = Measure("dx", domain=mesh)
