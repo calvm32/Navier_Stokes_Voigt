df5988a87907e981c3f9144c392bdd1596c64217
Calum Heldt <cal.muon32@gmail.com>
2025-12-28 14:46:47 -0700

    fix dimension:

--- solvers/navier_stokes_2d/solver_MMS.py
+++ solvers/navier_stokes_2d/solver_MMS.py
@@ -58,7 +58,10 @@ for N in N_list:
         ufl_p_exact = Constant(P)*x + Constant(G)
 
         # v_t
-        diff = Re*(-1*pi**2*t/(H**2))*(sin(y*pi/H)*e**(-1*pi**2*t/(H**2)))
+        diff = ([
+            Re*(-1*pi**2/(H**2))*(sin(y*pi/H)*e**(-1*pi**2*t/(H**2))), 
+            Constant(0.0)
+        ])
 
         # source term exact
         ufl_f_exact = ([
