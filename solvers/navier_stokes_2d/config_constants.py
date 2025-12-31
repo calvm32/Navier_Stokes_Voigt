91b0c07cd57cb419c5ae0f3d056128bc930ae557
Calum Heldt <cal.muon32@gmail.com>
2025-12-30 17:59:47 -0700

    remove another

--- solvers/navier_stokes_2d/config_constants.py
+++ solvers/navier_stokes_2d/config_constants.py
@@ -96,5 +96,5 @@ solver_parameters = {
 
     # finally make the pcd look at the velocity function for more refined meshes
 
-    "fieldsplit_1_pcd_u": "velocity",
+    #"fieldsplit_1_pcd_u": "velocity",
     }
