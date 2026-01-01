f682e983dba5a6ecf8d213c702bd5bbf34b8a1e4
Calum Heldt <cal.muon32@gmail.com>
2025-12-31 18:54:19 -0700

    make CFL dt=1/N

--- solvers/navier_stokes_2d/solver_MMS.py
+++ solvers/navier_stokes_2d/solver_MMS.py
@@ -13,7 +13,7 @@ p_error_list = []
 
 for N in N_list:
 
-    dt = 1/N**2 # CFL
+    dt = 1/N # CFL
 
     blue(f"\n*** Mesh size N = {N:0d} ***\n", spaced=True) # report mesh size
     new_vtkfile_name = f"{vtkfile_name}_N{N}" # write to new file
