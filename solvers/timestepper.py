e546d92a02e40855ddd223e7a02d4df6a3674bbe
Calum Heldt <cal.muon32@gmail.com>
2026-01-04 20:21:08 -0700

    some heat solns; now go for ns

--- solvers/timestepper.py
+++ solvers/timestepper.py
@@ -108,7 +108,10 @@ def timestepper(get_data, theta, Z, dx , dsN, t0, T, dt, make_weak_form,
 
         # write to VTK every 2 steps
         if step % 2 == 0:
-            outfile.write(u, time=t)
+            if isinstance(Z.ufl_element(), MixedElement):
+                outfile.write(u.sub(0), u.sub(1), time=t)
+            else:
+                outfile.write(u, time=t)
 
     # ----------------------------------
     # Report done; find and return error
