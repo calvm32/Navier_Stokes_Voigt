dfc68f4edeea1ee3887c1cde1c18e0d6aa3d45d0
Calum Heldt <cal.muon32@gmail.com>
2026-01-15 15:09:14 -0600

    dt 0.002

--- solvers/navier_stokes_2d/config_constants.py
+++ solvers/navier_stokes_2d/config_constants.py
@@ -6,7 +6,7 @@ from firedrake import *
 
 t0 = 0.0        # initial time
 T = 1           # final time
-dt = 0.01       # time step
+dt = 0.002       # time step
 theta = 0.5     # theta constant
 gamma = 0.0     # grad-div stabilization constant
 Re = 100.0      # Reynold's num = 1/viscostiy
