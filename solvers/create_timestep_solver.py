3ddb4baa841380ea42d593906a300ab8ae7c20d5
Calum Heldt <cal.muon32@gmail.com>
2026-01-14 20:24:19 -0600

    go back to old interpolates

--- solvers/create_timestep_solver.py
+++ solvers/create_timestep_solver.py
@@ -51,15 +51,15 @@ def create_timestep_solver(get_data, theta, Z, dx , dsN, u_old, u, make_weak_for
         data_new = get_data(t+dt)
 
         if is_mixed:
-            f.sub(0).assign(project(data_new["ufl_f"], f.sub(0).function_space()))
-            g.sub(0).assign(project(data_new["ufl_f"], g.sub(0).function_space()))
-            f_old.sub(0).assign(project(data_old["ufl_f"], f.sub(0).function_space()))
-            g_old.sub(0).assign(project(data_old["ufl_f"], g.sub(0).function_space()))
+            f.sub(0).interpolate(data_new["ufl_f"])
+            g.sub(0).interpolate(data_new["ufl_g"])
+            f_old.sub(0).interpolate(data_old["ufl_f"])
+            g_old.sub(0).interpolate(data_old["ufl_g"])
         else:
-            f.assign(project(data_new["ufl_f"], f.sub(0).function_space()))
-            g.assign(project(data_new["ufl_f"], g.sub(0).function_space()))
-            f_old.assign(project(data_old["ufl_f"], f.sub(0).function_space()))
-            g_old.assign(project(data_old["ufl_f"], g.sub(0).function_space()))
+            f.interpolate(data_new["ufl_f"])
+            g.interpolate(data_new["ufl_g"])
+            f_old.interpolate(data_old["ufl_f"])
+            g_old.interpolate(data_old["ufl_g"])
 
         # Run the solver
         solver.solve()
