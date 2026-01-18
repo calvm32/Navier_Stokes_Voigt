dea913893dbdabc31cf061192e051c29b382e285
Calum Heldt <cal.muon32@gmail.com>
2026-01-17 20:01:28 -0600

    v0=0, now test p0 nonzer

--- solvers/navier_stokes_2d/solver.py
+++ solvers/navier_stokes_2d/solver.py
@@ -81,12 +81,12 @@ def get_data(t):
 
     # velocity exact
     ufl_v0 = as_vector([
-        P*y*(y - H),
+        0.0, #P*y*(y - H),
         0.0
     ])
 
     # pressure exact
-    ufl_p0 = Constant(0.0)
+    ufl_p0 = P*x+G #Constant(0.0)
 
     # source termexact
     ufl_f0 = as_vector([0.0,0.0])
