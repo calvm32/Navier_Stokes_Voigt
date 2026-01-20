ad325ced9179271feb200c98efbb9173734e8743
Calum Heldt <cal.muon32@gmail.com>
2026-01-19 22:31:19 -0600

    back to nonzero stuff

--- solvers/navier_stokes_2d/solver.py
+++ solvers/navier_stokes_2d/solver.py
@@ -62,16 +62,16 @@ tol = 1e-2
 def get_data(t):
 
     # velocity exact
-    ufl_v0 = as_vector([0.0,0.0])
+    ufl_v0 = as_vector([P*y*(y - H),0.0])
 
     # pressure exact
-    ufl_p0 = Constant(0.0)
+    ufl_p0 = P*x + G
 
     # source termexact
-    ufl_f0 = as_vector([G,0.0])
+    ufl_f0 = as_vector([0.0,0.0])
 
     # boundary term
-    ufl_g0 = as_vector([0.0,0.0]) #as_vector([P*exp(-(x/tol)**2), 0.0])
+    ufl_g0 = as_vector([P*exp(-(x/tol)**2), 0.0])
 
     return {
         "ufl_v0": ufl_v0,
