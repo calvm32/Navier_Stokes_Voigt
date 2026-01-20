6b61392bf04017cf3a255754b703eb96d4a54eaa
Calum Heldt <cal.muon32@gmail.com>
2026-01-19 22:31:55 -0600

    try diff g0

--- solvers/navier_stokes_2d/solver.py
+++ solvers/navier_stokes_2d/solver.py
@@ -71,7 +71,7 @@ def get_data(t):
     ufl_f0 = as_vector([0.0,0.0])
 
     # boundary term
-    ufl_g0 = as_vector([P*exp(-(x/tol)**2), 0.0])
+    ufl_g0 = as_vector([(L-x)*G/L - x*(P*L-G)/L, 0.0]) #as_vector([P*exp(-(x/tol)**2), 0.0])
 
     return {
         "ufl_v0": ufl_v0,
