3663289276bb02286296b924fc584cfc351f8366
Calum Heldt <cal.muon32@gmail.com>
2026-01-25 20:16:38 -0600

    try much easer version

--- solvers/navier_stokes_2d/solver_SV.py
+++ solvers/navier_stokes_2d/solver_SV.py
@@ -71,7 +71,8 @@ print(f"[solver.py] YAML configs archived in {run_dir}\n")
 # ------------
 
 # Load the mesh
-mesh = Mesh(MESH_PATH)
+#mesh = Mesh(MESH_PATH)
+mesh = RectangleMesh(64, 16, 4, 1)
 mesh_hierarchy = MeshHierarchy(mesh, 1)  # 1 = one barycenter refinement
 fine_mesh = mesh_hierarchy[-1]
 
