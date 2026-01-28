7546b6c0d396d71b0e5f2e281eed80f8eff815bc
Calum Heldt <cal.muon32@gmail.com>
2026-01-27 23:03:58 -0600

    fix energy plot bugs

--- solvers/heat_2d/solver.py
+++ solvers/heat_2d/solver.py
@@ -79,7 +79,7 @@ def get_data(t):
 # Run solver
 # ----------
 
-u_error_list, palinstrophy_list, stream_func_list, enstrophy_list, time_list = timestepper(get_data, 
+u_error_list, palinstrophy_list, stream_func_list, enstrophy_list, time_list, energy_list, all_time_list = timestepper(get_data, 
         V, dx, ds, 
         t0, T, dt,
         make_weak_form=make_weak_form,
@@ -122,9 +122,9 @@ plt.tight_layout()
 plt.savefig("0_enstrophy_plot.png", dpi=200, bbox_inches='tight')
 plt.close()
 
-# --------------
-# Plot Enstrophy
-# --------------
+# -----------
+# Plot Energy
+# -----------
 
 plt.loglog(all_time_list, energy_list, "-o")
 plt.xlabel("time")
