c057f5cf8cc0d201a499972c6dc58432989fbffd
Calum Heldt <cal.muon32@gmail.com>
2026-01-29 22:16:12 -0600

    try to interpret sps

--- solvers/timestepper.py
+++ solvers/timestepper.py
@@ -10,7 +10,7 @@ def timestepper(get_data, Z, dx , dsN, t0, T, dt, make_weak_form,
     Crank-Nicolson theta-scheme timestepper for velocity or velocity x pressure function spaces
     """
 
-    num_steps = int((float(T)-float(t0)) / float(dt)) + 1 # add 1 to include first pt
+    num_steps = int((float(T)-float(t0)) / float(dt)) 
     is_mixed = isinstance(Z.ufl_element(), MixedElement)
     compute_every = 5
 
