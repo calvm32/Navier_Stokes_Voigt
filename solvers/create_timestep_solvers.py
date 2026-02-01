156beb26773103f54e0c264c3e878a6b2f9118dc
Calum Heldt <cal.muon32@gmail.com>
2026-02-01 13:53:59 -0600

    keep updating nullspace

--- solvers/create_timestep_solvers.py
+++ solvers/create_timestep_solvers.py
@@ -101,6 +101,9 @@ def create_timestep_solver_BDF2(get_data, Z, dx , dsN, u_older, u_old, u, make_w
             nullspace=nullspace,
             appctx=appctx
         )
+        ksp = solver.snes.ksp
+        ksp.setNullSpace(nullspace)
+        ksp.setNearNullSpace(nullspace)
     else:
         a, L = make_weak_form(
             idt, f, f_old, g, g_old, u_older, u_old, dx, dsN
