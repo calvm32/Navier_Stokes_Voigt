88e36d4ae51e4ea6daf9d59b2a99d3235a39addb
Calum Heldt <cal.muon32@gmail.com>
2026-02-01 23:11:07 -0600

    omega <- vorticity

--- solvers/navier_stokes_2d/solver.py
+++ solvers/navier_stokes_2d/solver.py
@@ -270,7 +270,7 @@ plt.tight_layout()
 plt.savefig("0_velocity_PDF.png", dpi=200, bbox_inches='tight')
 plt.close()
 
-plt.hist(vorticity_vals, bins=100, density=True)
+plt.hist(omega_vals, bins=100, density=True)
 plt.xlabel("samples")
 plt.ylabel("vorticity")
 plt.grid(True)
