0e4c4ca396eefc77cad8ff228bce9ef8d3dfaaf8
Calum Heldt <cal.muon32@gmail.com>
2026-02-24 15:17:11 -0600

    slight v block sps change for boomergang

--- solvers/navier_stokes_2d/solver.py
+++ solvers/navier_stokes_2d/solver.py
@@ -118,8 +118,8 @@ elif elements == "TH":
     W = FunctionSpace(fine_mesh, "CG", 1)
     Z = V * W
 
-print(f"// V Total DoFs: {V.dof_count}")
-print(f"// W Total DoFs: {W.dof_count}")
+# print(f"// V Total DoFs: {V.dof_count}")
+# print(f"// W Total DoFs: {W.dof_count}")
 
 # -------------------
 # Configure functions
