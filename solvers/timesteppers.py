add2b63bc87568baab0b88aa18176b352a17b8fe
Calum Heldt <cal.muon32@gmail.com>
2026-02-28 14:04:46 -0600

    fix errs

--- solvers/timesteppers.py
+++ solvers/timesteppers.py
@@ -87,8 +87,8 @@ def timestepper_CN(get_data, Z, dx , dsN, t0, T, dt, make_weak_form, sample_leng
     target = np.array(energy_spec_target)
 
     # DOF coordinates (parallel safe)
-    V = Z.sub(0).collapse()
-    coords = V.tabulate_dof_coordinates().reshape(-1, 2)
+    V_real = FunctionSpace(mesh, Z.ufl_element().sub_elements()[0])
+    coords = V_real.tabulate_dof_coordinates().reshape(-1, 2)
 
     local_min_dist = 1e20
     local_index = -1
@@ -385,8 +385,8 @@ def timestepper_BDF2(get_data, Z, dx , dsN, t0, T, dt, make_weak_form_BDF2, make
     target = np.array(energy_spec_target)
 
     # DOF coordinates (parallel safe)
-    V = Z.sub(0).collapse()
-    coords = V.tabulate_dof_coordinates().reshape(-1, 2)
+    V_real = FunctionSpace(mesh, Z.ufl_element().sub_elements()[0])
+    coords = V_real.tabulate_dof_coordinates().reshape(-1, 2)
 
     local_min_dist = 1e20
     local_index = -1
