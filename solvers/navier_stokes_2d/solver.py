3bd62e7a319fc6b6dd17db931085f0c74b728eeb
Calum Heldt <cal.muon32@gmail.com>
2026-03-04 23:55:17 -0600

    actually fix things :/

--- solvers/navier_stokes_2d/solver.py
+++ solvers/navier_stokes_2d/solver.py
@@ -380,9 +380,9 @@ def main(save_dir):
         plt.loglog(k_plot, E_plot, label="Energy spectrum")
 
         # Reference slope
-        if len(k) > 6:
-            C = E_k[5] * k[5]**(5/3)
-            plt.loglog(k, C * k**(-5/3), '--', label=r"$k^{-5/3}$")
+        if len(k_plot) > 6:
+            C = E_plot[5] * k_plot[5]**(5/3)
+            plt.loglog(k_plot, C * k_plot**(-5/3), '--', label=r"$k^{-5/3}$")
 
         plot_data["energy_spec_probe"] = (k, E_k)
         plt.xlabel("Wavenumber k")
