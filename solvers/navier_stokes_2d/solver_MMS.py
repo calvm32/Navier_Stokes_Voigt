af94928ea38c1aadabdc14719aec863e15e72dd6
Calum Heldt <cal.muon32@gmail.com>
2026-03-10 20:03:12 -0500

    solver_MMS same calls as solver for ns2d

--- solvers/navier_stokes_2d/solver_MMS.py
+++ solvers/navier_stokes_2d/solver_MMS.py
@@ -157,9 +157,9 @@ def main(save_dir):
         # ----------
 
         if solver == "CN":
-            v_error_list, p_error_list, palinstrophy_list, stream_func_list, enstrophy_list, every_time_list, energy_list, all_time_list, velocity_x_vals, velocity_y_vals, omega_vals, r_vals, S2, energy_spec_list, energy_spec_probe, compute_every_large = timestepper_CN(get_data, 
+            v_error_list, p_error_list, palinstrophy_list, stream_func_list, enstrophy_list, every_time_list, energy_list, all_time_list, velocity_x_vals, velocity_y_vals, omega_vals, r_vals, S2, energy_spec_probe = timestepper_CN(get_data, 
                     Z, dx, ds, 
-                    t0, T, dt, theta=theta, gamma=gamma, Re=Re,
+                    t0, T, dt,
                     sample_length=L, sample_height=H,
                     make_weak_form=make_weak_form_CN, 
                     bcs=bcs, nullspace=nullspace,
@@ -167,9 +167,9 @@ def main(save_dir):
                     appctx=appctx, vtkfile_name=vtkfile_name)
 
         elif solver == "BDF2":
-            v_error_list, p_error_list, palinstrophy_list, stream_func_list, enstrophy_list, every_time_list, energy_list, all_time_list, velocity_x_vals, velocity_y_vals, omega_vals, r_vals, S2, energy_spec_list, energy_spec_probe, compute_every_large = timestepper_BDF2(get_data, 
+            v_error_list, p_error_list, palinstrophy_list, stream_func_list, enstrophy_list, every_time_list, energy_list, all_time_list, velocity_x_vals, velocity_y_vals, omega_vals, r_vals, S2, energy_spec_probe = timestepper_BDF2(get_data, 
                     Z, dx, ds, 
-                    t0, T, dt, gamma=gamma, Re=Re,
+                    t0, T, dt, 
                     sample_length=L, sample_height=H,
                     make_weak_form_BDF2=make_weak_form_BDF2,
                     make_weak_form_CN=make_weak_form_CN,
