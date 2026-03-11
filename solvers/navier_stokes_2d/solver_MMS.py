5f6469e25c85965f6b92d9e3b10827ecd35cff65
Calum Heldt <cal.muon32@gmail.com>
2026-03-10 21:53:41 -0500

    exact same bcs for mms

--- solvers/navier_stokes_2d/solver_MMS.py
+++ solvers/navier_stokes_2d/solver_MMS.py
@@ -173,8 +173,13 @@ def main(save_dir):
         # Boundary conditions
         # -------------------
 
-        bcs = [DirichletBC(Z.sub(0), Constant((0.0, 0.0)), (3, 4))]
-        nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0), VectorSpaceBasis(constant=True)])
+        u_inflow = ufl_cfg["ufl_v0"]
+
+        bc_inflow = DirichletBC(Z.sub(0), u_inflow, (1,2))
+        bc_walls = DirichletBC(Z.sub(0), Constant((0.0, 0.0)), (3,4))
+
+        bcs = [bc_walls, bc_inflow]
+        nullspace = MixedVectorSpaceBasis(Z, [Z.sub(0), VectorSpaceBasis(constant=True, comm=Z.mesh().comm)])
 
         # ------------------
         # Allocate functions
