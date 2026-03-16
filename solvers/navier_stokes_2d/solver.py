c854c05a7f7a438336eed357488a3968e2080bb7
Calum Heldt <cal.muon32@gmail.com>
2026-03-15 20:20:48 -0500

    test bary4

--- solvers/navier_stokes_2d/solver.py
+++ solvers/navier_stokes_2d/solver.py
@@ -93,7 +93,7 @@ def main(save_dir):
     # --------------
 
     HERE = os.path.dirname(os.path.abspath(__file__))
-    MESH_PATH = os.path.join(HERE, "meshes/mms", "channel_bary1.msh")
+    MESH_PATH = os.path.join(HERE, "meshes/mms", "channel_bary4.msh")
 
     # print(f"[solver.py] Loading mesh from: {MESH_PATH}")
 
