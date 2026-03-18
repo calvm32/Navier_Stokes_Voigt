5068dd4d612fd39deca98088c319e048e27aea2f
Calum Heldt <cal.muon32@gmail.com>
2026-03-18 14:03:14 -0500

    distortion measure

--- solvers/heat_2d/solver.py
+++ solvers/heat_2d/solver.py
@@ -14,28 +14,6 @@ import numpy as np
 
 from mpi4py import MPI
 
-# ---------
-# Get paths
-# ---------
-
-def load_run_configs(save_dir):
-    save_dir = Path(save_dir)
-    cfg_path = save_dir / "settings.yaml"
-    solver_params_path = save_dir / "solver_params.yaml"
-
-    cfg = load_config(cfg_path)
-    solver_parameters = load_solver_parameters(solver_params_path)
-
-    return cfg, solver_parameters
-
-def load_run_ufls(save_dir, namespace):
-    save_dir = Path(save_dir)
-    ufl_path = save_dir / "ufl_expr.yaml"
-    
-    ufl_cfg = load_ufl_expressions(ufl_path, namespace=namespace)
-
-    return ufl_cfg
-
 def main(save_dir):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
--- solvers/heat_2d/solver_MMS.py
+++ solvers/heat_2d/solver_MMS.py
@@ -11,28 +11,6 @@ from .make_weak_form import *
 from solvers.processing.printoff import blue, green
 from solvers.processing.config_setup import *
 
-# ---------
-# Get paths
-# ---------
-
-def load_run_configs(save_dir):
-    save_dir = Path(save_dir)
-    cfg_path = save_dir / "settings.yaml"
-    solver_params_path = save_dir / "solver_params.yaml"
-
-    cfg = load_config(cfg_path)
-    solver_parameters = load_solver_parameters(solver_params_path)
-
-    return cfg, solver_parameters
-
-def load_run_ufls(save_dir, namespace):
-    save_dir = Path(save_dir)
-    ufl_path = save_dir / "ufl_expr.yaml"
-    
-    ufl_cfg = load_ufl_expressions(ufl_path, namespace=namespace)
-
-    return ufl_cfg
-
 def main(save_dir):
     
     comm = MPI.COMM_WORLD
