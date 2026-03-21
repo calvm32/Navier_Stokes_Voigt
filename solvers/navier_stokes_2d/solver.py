5c82bf07a8e39f8d2cc23fa948f827b37da4e5e5
Calum Heldt <cal.muon32@gmail.com>
2026-03-21 15:29:01 -0500

    test bary1 again

--- solvers/navier_stokes_2d/solver.py
+++ solvers/navier_stokes_2d/solver.py
@@ -73,7 +73,7 @@ def main(save_dir):
     # --------------
 
     HERE = os.path.dirname(os.path.abspath(__file__))
-    MESH_PATH = os.path.join(HERE, "meshes/mms", "channel.msh")
+    MESH_PATH = os.path.join(HERE, "meshes/mms", "channel_bary1.msh")
 
     # ------------
     # Setup spaces
