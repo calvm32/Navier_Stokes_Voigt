abf5b9f12cfdde6858d995e26b62b2384149ba41
Calum Heldt <cal.muon32@gmail.com>
2026-05-04 13:31:27 -0500

    make weak form typo (had rhs)

--- navier_stokes_voigt/solvers_FEM/compare_2d/make_weak_form.py
+++ navier_stokes_voigt/solvers_FEM/compare_2d/make_weak_form.py
@@ -3,259 +3,258 @@ from firedrake import *
 def voigt_inner(u, v, alpha):
     return inner(u, v) + alpha**2 * inner(grad(u), grad(v))
 
-def make_rhs(kx, ky, dealias, Re, inv_lap):
 
-    def make_weak_form_NSE_BDF2(idt, f, f_old, g, g_old, U_older, U_old, dx, dsN, gamma, Re):
-        """
-        BDF2 Navier-Stokes
-        - Oseen linearization
-        - skew-symmetric convection
-        - implicit viscosity
-        - implicit grad-div
-        """
+def make_weak_form_NSE_BDF2(idt, f, f_old, g, g_old, U_older, U_old, dx, dsN, gamma, Re):
+    """
+    BDF2 Navier-Stokes
+    - Oseen linearization
+    - skew-symmetric convection
+    - implicit viscosity
+    - implicit grad-div
+    """
 
-        # Extrapolated forcing
-        f_bdf2 = 2.0*f.sub(0) - f_old.sub(0)
-        g_bdf2 = 2.0*g.sub(0) - g_old.sub(0)
+    # Extrapolated forcing
+    f_bdf2 = 2.0*f.sub(0) - f_old.sub(0)
+    g_bdf2 = 2.0*g.sub(0) - g_old.sub(0)
 
-        # Old velocities
-        u_old,  _ = split(U_old)
-        u_older, _ = split(U_older)
+    # Old velocities
+    u_old,  _ = split(U_old)
+    u_older, _ = split(U_older)
 
-        def forms(U, V):
-            u, p = split(U)
-            v, q = split(V)
+    def forms(U, V):
+        u, p = split(U)
+        v, q = split(V)
 
-            # ------
-            # LHS
-            # ------
-            a = (
-                # BDF2 mass
-                (3.0 / 2.0) * idt * inner(u, v) * dx
+        # ------
+        # LHS
+        # ------
+        a = (
+            # BDF2 mass
+            (3.0 / 2.0) * idt * inner(u, v) * dx
 
-                # Skew-symmetric Oseen advection
-                + 0.5 * (
-                    inner(dot(u_old, nabla_grad(u)), v)
-                - inner(dot(u_old, nabla_grad(v)), u)
-                ) * dx
+            # Skew-symmetric Oseen advection
+            + 0.5 * (
+                inner(dot(u_old, nabla_grad(u)), v)
+            - inner(dot(u_old, nabla_grad(v)), u)
+            ) * dx
 
-                # Viscosity
-                + (1.0 / Re) * inner(grad(u), grad(v)) * dx
+            # Viscosity
+            + (1.0 / Re) * inner(grad(u), grad(v)) * dx
 
-                # Pressure coupling
-                - p * div(v) * dx
-                - q * div(u) * dx
-            )
+            # Pressure coupling
+            - p * div(v) * dx
+            - q * div(u) * dx
+        )
 
-            # Grad-div
-            if gamma != 0.0:
-                a += gamma * inner(div(u), div(v)) * dx
+        # Grad-div
+        if gamma != 0.0:
+            a += gamma * inner(div(u), div(v)) * dx
 
-            # ------
-            # RHS
-            # ------
-            L = (
-                # BDF2 history
-                0.5 * idt * inner(4.0*u_old - u_older, v) * dx
+        # ------
+        # RHS
+        # ------
+        L = (
+            # BDF2 history
+            0.5 * idt * inner(4.0*u_old - u_older, v) * dx
 
-                # Forcing
-                + inner(f_bdf2, v) * dx
+            # Forcing
+            + inner(f_bdf2, v) * dx
 
-                # Neumann BC
-                + inner(g_bdf2, v) * dsN
-            )
+            # Neumann BC
+            + inner(g_bdf2, v) * dsN
+        )
 
-            return a, L
+        return a, L
 
-        return forms
+    return forms
 
-    def make_weak_form_NSV_BDF2(idt, f, f_old, g, g_old, U_older, U_old, dx, dsN, gamma, Re, alpha):
-        """
-        BDF2 Navier-Stokes-Voigt
-        - Oseen linearization
-        - skew-symmetric convection
-        - implicit viscosity
-        - implicit grad-div
-        """
+def make_weak_form_NSV_BDF2(idt, f, f_old, g, g_old, U_older, U_old, dx, dsN, gamma, Re, alpha):
+    """
+    BDF2 Navier-Stokes-Voigt
+    - Oseen linearization
+    - skew-symmetric convection
+    - implicit viscosity
+    - implicit grad-div
+    """
 
-        # Extrapolated forcing
-        f_bdf2 = 2.0*f.sub(0) - f_old.sub(0)
-        g_bdf2 = 2.0*g.sub(0) - g_old.sub(0)
+    # Extrapolated forcing
+    f_bdf2 = 2.0*f.sub(0) - f_old.sub(0)
+    g_bdf2 = 2.0*g.sub(0) - g_old.sub(0)
 
-        # Old velocities
-        u_old,  _ = split(U_old)
-        u_older, _ = split(U_older)
+    # Old velocities
+    u_old,  _ = split(U_old)
+    u_older, _ = split(U_older)
 
-        def forms(U, V):
-            u, p = split(U)
-            v, q = split(V)
+    def forms(U, V):
+        u, p = split(U)
+        v, q = split(V)
 
-            # Bilinear form a(U,V)
-            a = (
-                # BDF2 mass
-                (3.0 / 2.0) * idt * voigt_inner(u, v, alpha) * dx # CHANGED THIS FOR VOIGT
+        # Bilinear form a(U,V)
+        a = (
+            # BDF2 mass
+            (3.0 / 2.0) * idt * voigt_inner(u, v, alpha) * dx # CHANGED THIS FOR VOIGT
 
-                # Skew-symmetric Oseen advection
-                + 0.5 * (
-                    inner(dot(u_old, nabla_grad(u)), v)
-                - inner(dot(u_old, nabla_grad(v)), u)
-                ) * dx
+            # Skew-symmetric Oseen advection
+            + 0.5 * (
+                inner(dot(u_old, nabla_grad(u)), v)
+            - inner(dot(u_old, nabla_grad(v)), u)
+            ) * dx
 
-                # Viscosity
-                + (1.0 / Re) * inner(grad(u), grad(v)) * dx
+            # Viscosity
+            + (1.0 / Re) * inner(grad(u), grad(v)) * dx
 
-                # Pressure coupling
-                - p * div(v) * dx
-                - q * div(u) * dx
-            )
+            # Pressure coupling
+            - p * div(v) * dx
+            - q * div(u) * dx
+        )
 
-            # Grad-div
-            if gamma != 0.0:
-                a += gamma * inner(div(u), div(v)) * dx
+        # Grad-div
+        if gamma != 0.0:
+            a += gamma * inner(div(u), div(v)) * dx
 
-            # Linear form L(V)
-            L = (
-                # BDF2 history
-                0.5 * idt * inner(4.0*u_old - u_older, v) * dx
+        # Linear form L(V)
+        L = (
+            # BDF2 history
+            0.5 * idt * inner(4.0*u_old - u_older, v) * dx
 
-                # Forcing
-                + inner(f_bdf2, v) * dx
+            # Forcing
+            + inner(f_bdf2, v) * dx
 
-                # Neumann BC
-                + inner(g_bdf2, v) * dsN
-            )
+            # Neumann BC
+            + inner(g_bdf2, v) * dsN
+        )
 
-            return a, L
+        return a, L
 
-        return forms
+    return forms
 
-    
-    def make_weak_form_NSE_CN(idt, f, f_old, g, g_old, U_old, dx, dsN, theta, gamma, Re):
-        """
-        Crank-Nicolson Navier-Stokes-Voigt
-        -> Oseen linearization
-        -> grad-div stabilization
-        -> bilinear, linear
-        """
 
-        # Midpoints
-        f_mid = theta*f.sub(0) + (1.0 - theta)*f_old.sub(0)
-        g_mid = theta*g.sub(0) + (1.0 - theta)*g_old.sub(0)
+def make_weak_form_NSE_CN(idt, f, f_old, g, g_old, U_old, dx, dsN, theta, gamma, Re):
+    """
+    Crank-Nicolson Navier-Stokes-Voigt
+    -> Oseen linearization
+    -> grad-div stabilization
+    -> bilinear, linear
+    """
 
-        u_old = U_old.sub(0)
-        p_old = U_old.sub(1)
+    # Midpoints
+    f_mid = theta*f.sub(0) + (1.0 - theta)*f_old.sub(0)
+    g_mid = theta*g.sub(0) + (1.0 - theta)*g_old.sub(0)
 
-        def forms(U, V):
-            u, p = split(U)
-            v, q = split(V)
+    u_old = U_old.sub(0)
+    p_old = U_old.sub(1)
 
-            # Bilinear form a(U,V)
-            a = (
-                # Time derivative
-                idt * inner(u, v) * dx
+    def forms(U, V):
+        u, p = split(U)
+        v, q = split(V)
 
-                # Skew-symmetric Oseen advection
-                + 0.5 * (
-                    inner(dot(u_old, nabla_grad(u)), v)
-                - inner(dot(u_old, nabla_grad(v)), u)
-                ) * dx
+        # Bilinear form a(U,V)
+        a = (
+            # Time derivative
+            idt * inner(u, v) * dx
 
-                # Viscosity
-                + (theta / Re) * inner(grad(u), grad(v)) * dx
+            # Skew-symmetric Oseen advection
+            + 0.5 * (
+                inner(dot(u_old, nabla_grad(u)), v)
+            - inner(dot(u_old, nabla_grad(v)), u)
+            ) * dx
 
-                # Pressure / continuity
-                - p * div(v) * dx
-                - q * div(u) * dx
-            )
+            # Viscosity
+            + (theta / Re) * inner(grad(u), grad(v)) * dx
 
-            # Grad–div stabilization
-            if gamma != 0:
-                a += theta * gamma * inner(div(u), div(v)) * dx
+            # Pressure / continuity
+            - p * div(v) * dx
+            - q * div(u) * dx
+        )
 
-            # Linear form L(V)
-            L = (
-                # Time derivative
-                idt * inner(u_old, v) * dx
+        # Grad–div stabilization
+        if gamma != 0:
+            a += theta * gamma * inner(div(u), div(v)) * dx
 
-                # Explicit viscosity
-                - ((1.0 - theta) / Re) * inner(grad(u_old), grad(v)) * dx
+        # Linear form L(V)
+        L = (
+            # Time derivative
+            idt * inner(u_old, v) * dx
 
-                # Forcing
-                + inner(f_mid, v) * dx
+            # Explicit viscosity
+            - ((1.0 - theta) / Re) * inner(grad(u_old), grad(v)) * dx
 
-                # Neumann boundary
-                + inner(g_mid, v) * dsN
-            )
+            # Forcing
+            + inner(f_mid, v) * dx
 
-            # Grad–div stabilization
-            if gamma != 0:
-                L -= (1.0 - theta) * gamma * inner(div(u_old), div(v)) * dx
+            # Neumann boundary
+            + inner(g_mid, v) * dsN
+        )
 
-            return a, L
+        # Grad–div stabilization
+        if gamma != 0:
+            L -= (1.0 - theta) * gamma * inner(div(u_old), div(v)) * dx
 
-        return forms
+        return a, L
 
-    def make_weak_form_NSV_CN(idt, f, f_old, g, g_old, U_old, dx, dsN, theta, gamma, Re, alpha):
-        """
-        Crank-Nicolson Navier-Stokes-Voigt
-        -> Oseen linearization
-        -> grad-div stabilization
-        -> bilinear, linear
-        """
+    return forms
 
-        # Midpoints
-        f_mid = theta*f.sub(0) + (1.0 - theta)*f_old.sub(0)
-        g_mid = theta*g.sub(0) + (1.0 - theta)*g_old.sub(0)
+def make_weak_form_NSV_CN(idt, f, f_old, g, g_old, U_old, dx, dsN, theta, gamma, Re, alpha):
+    """
+    Crank-Nicolson Navier-Stokes-Voigt
+    -> Oseen linearization
+    -> grad-div stabilization
+    -> bilinear, linear
+    """
 
-        u_old = U_old.sub(0)
-        p_old = U_old.sub(1)
+    # Midpoints
+    f_mid = theta*f.sub(0) + (1.0 - theta)*f_old.sub(0)
+    g_mid = theta*g.sub(0) + (1.0 - theta)*g_old.sub(0)
 
-        def forms(U, V):
-            u, p = split(U)
-            v, q = split(V)
+    u_old = U_old.sub(0)
+    p_old = U_old.sub(1)
 
-            # Bilinear form a(U,V)
-            a = (
-                # Time derivative
-                idt * voigt_inner(u, v, alpha) * dx # CHANGED THIS FOR VOIGT
+    def forms(U, V):
+        u, p = split(U)
+        v, q = split(V)
 
-                # Skew-symmetric Oseen advection
-                + 0.5 * (
-                    inner(dot(u_old, nabla_grad(u)), v)
-                - inner(dot(u_old, nabla_grad(v)), u)
-                ) * dx
+        # Bilinear form a(U,V)
+        a = (
+            # Time derivative
+            idt * voigt_inner(u, v, alpha) * dx # CHANGED THIS FOR VOIGT
 
-                # Viscosity
-                + (theta / Re) * inner(grad(u), grad(v)) * dx
+            # Skew-symmetric Oseen advection
+            + 0.5 * (
+                inner(dot(u_old, nabla_grad(u)), v)
+            - inner(dot(u_old, nabla_grad(v)), u)
+            ) * dx
 
-                # Pressure / continuity
-                - p * div(v) * dx
-                - q * div(u) * dx
-            )
+            # Viscosity
+            + (theta / Re) * inner(grad(u), grad(v)) * dx
 
-            # Grad–div stabilization
-            if gamma != 0:
-                a += theta * gamma * inner(div(u), div(v)) * dx
+            # Pressure / continuity
+            - p * div(v) * dx
+            - q * div(u) * dx
+        )
 
-            # Linear form L(V)
-            L = (
-                # Time derivative
-                idt * inner(u_old, v) * dx
+        # Grad–div stabilization
+        if gamma != 0:
+            a += theta * gamma * inner(div(u), div(v)) * dx
 
-                # Explicit viscosity
-                - ((1.0 - theta) / Re) * inner(grad(u_old), grad(v)) * dx
+        # Linear form L(V)
+        L = (
+            # Time derivative
+            idt * inner(u_old, v) * dx
 
-                # Forcing
-                + inner(f_mid, v) * dx
+            # Explicit viscosity
+            - ((1.0 - theta) / Re) * inner(grad(u_old), grad(v)) * dx
 
-                # Neumann boundary
-                + inner(g_mid, v) * dsN
-            )
+            # Forcing
+            + inner(f_mid, v) * dx
 
-            # Grad–div stabilization
-            if gamma != 0:
-                L -= (1.0 - theta) * gamma * inner(div(u_old), div(v)) * dx
+            # Neumann boundary
+            + inner(g_mid, v) * dsN
+        )
 
-            return a, L
+        # Grad–div stabilization
+        if gamma != 0:
+            L -= (1.0 - theta) * gamma * inner(div(u_old), div(v)) * dx
 
-        return forms
+        return a, L
+
+    return forms
