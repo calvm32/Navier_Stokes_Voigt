1a7b6047d4d2d0dfe9b1c1d00b104c7da0f95f07
Calum Heldt <cal.muon32@gmail.com>
2026-05-12 17:17:49 -0500

    fix dt

--- navier_stokes_voigt/solvers_FEM/navier_stokes_voigt_2d/solver_MMS.py
+++ navier_stokes_voigt/solvers_FEM/navier_stokes_voigt_2d/solver_MMS.py
@@ -80,7 +80,7 @@ def main(save_dir):
         blue(f"\n*** Mesh size N = {N:0d} ***", spaced=True) # report mesh size
         new_vtkfile_name = f"{vtkfile_name}_N{N}" # write to new file
 
-        dt = 0.1 / N**2
+        dt = 1/N
 
         # Build appctx
         appctx = {
