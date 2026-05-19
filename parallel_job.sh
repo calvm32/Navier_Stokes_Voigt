4053f3ccd7c396c4cd14a23270e0e8cda3432e62
Calum Heldt <cal.muon32@gmail.com>
2026-05-18 21:29:37 -0500

    update job req

--- parallel_job.sh
+++ parallel_job.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=nsv2_firedrake
 #SBATCH --output=job.out
 #SBATCH --error=job.err
-#SBATCH --nodes=1
-#SBATCH --ntasks=14
-#SBATCH --time=01:00:00
+#SBATCH --nodes=2
+#SBATCH --ntasks=32
+#SBATCH --time=10:00:00
 
 module purge
 module load apptainer compiler/gcc/11 openmpi/4.1
@@ -26,6 +26,10 @@ MMS=no # yes or no
 # ENVIRONMENT SETUP
 # -----------------
 
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+RUN_NAME="test_run_${TIMESTAMP}"
+
+IMAGE=docker://firedrakeproject/firedrake:2025.10.4
 SOLUTIONS_DIR=$PROJECT_DIR/solutions
 RUN_DIR=$SOLUTIONS_DIR/$RUN_NAME
 
@@ -35,6 +39,22 @@ export PYTHONPATH=$PROJECT_DIR:$PYTHONPATH
 export OMPI_MCA_opal_cuda_support=false
 
 mkdir -p "$SOLUTIONS_DIR"
+
+# ------------------------
+# INSTALL PROJECT PACKAGE
+# ------------------------
+
+apptainer exec \
+    --bind $PROJECT_DIR:$PROJECT_DIR \
+    --pwd $PROJECT_DIR \
+    $IMAGE \
+    bash -c "
+        pip install --user -e .
+        pip install --user -r requirements.txt
+        export PATH=\$HOME/.local/bin:\$PATH
+        which mysave
+        which myrun
+    "
 
 # --------------------
 # CREATE RUN DIRECTORY
@@ -62,12 +82,15 @@ if [ ! -d "$RUN_DIR" ]; then
 
     echo "Creating run directory:"
     echo "$CREATE_CMD"
-
+	
     apptainer exec \
-        --bind $PROJECT_DIR:$PROJECT_DIR \
-        --pwd $PROJECT_DIR \
-        docker://firedrakeproject/firedrake:2025.10.4 \
-        bash -c "$CREATE_CMD"
+    	--bind $PROJECT_DIR:$PROJECT_DIR \
+    	--pwd $PROJECT_DIR \
+    	$IMAGE \
+    	bash -c "
+    	    export PATH=\$HOME/.local/bin:\$PATH
+       	    $CREATE_CMD
+    	"
 fi
 
 # --------------
@@ -79,5 +102,8 @@ cd "$RUN_DIR" || exit 1
 apptainer exec \
     --bind $PROJECT_DIR:$PROJECT_DIR \
     --pwd "$RUN_DIR" \
-    docker://firedrakeproject/firedrake:2025.10.4 \
-    myrun --np $SLURM_NTASKS .
+    $IMAGE \
+    bash -c "
+        export PATH=\$HOME/.local/bin:\$PATH
+        myrun .
+    "
